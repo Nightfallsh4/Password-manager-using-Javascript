7f995da949a731e95049af80a83f306c931d8d70
Shanmugadevan <shanmugadevan@protonmail.com>
2022-03-29 11:54:11 +0530

    Added KeyDerivation and encryptAES functions in JS

--- passwords.js
+++ passwords.js
@@ -1,3 +1,23 @@
-function submit(){
-    console.log("Submit button clicked!!")
+function keyDerivation(password){
+    let salt = CryptoJS.lib.WordArray.random(256 / 8) // Salt
+    console.log("Salt- "+ salt)
+    let sha256 = CryptoJS.algo.SHA256.create()
+    let key256Bits = CryptoJS.PBKDF2(
+        password, 
+        salt.toString(), 
+        {
+            keySize: 128 / 32, 
+            iterations:1000, 
+            hasher: sha256
+        }) //Password based key derivation
+      console.log("Key- "+ key256Bits)
+      return key256Bits
 }
+
+function encryptAES(key, plainText){
+    let encryptedText = CryptoJS.AES.encrypt(plainText,key.toString())
+    console.log(encryptedText)
+    return encryptedText
+}
+key = keyDerivation("Thisisapassword!")
+encryptedText = encryptAES(key, "Hello my name is shanmuga")
