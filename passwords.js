1cdda1f16aabe511ec3ccad4d667a212a32aced6
Shanmugadevan <shanmugadevan@protonmail.com>
2022-03-29 16:57:19 +0530

    Added developement roadmap

--- passwords.js
+++ passwords.js
@@ -26,13 +26,8 @@ function decryptAES(key, cipherText){
     return plainText
 }
 
-// let key = keyDerivation("Thisisapassword!")
-// let encryptedText = encryptAES(key, "Hello my name is shanmuga")
-// let text = decryptAES(key, encryptedText)
-
-
-function passwordGenerate(passwordLength){
-    // let data  = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,;:'"{}[]()-_=+*/&^%$#@!~?<>`.split("")
+function passwordGenerate(passwordLength=15){
+    // Returns a cryptographic random number generated password
     data = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ',', ';', ':', "'", '"', '{', '}', '[', ']', '(', ')', '-', '_', '=', '+', 
@@ -52,8 +47,16 @@ function passwordGenerate(passwordLength){
     }
     console.log(password)
     console.log(password.length)
+    return password
 }
 
-passwordGenerate(25)
+// Take in master password and use it to try and decrypt the databade of the given username
+// Render the decrypted database with each accounts username and password with can be clicked to be copied
+// Add a lock button which will lock the database
+// Add a 'add' button which can be used to add an account, passwords of the accounts will be encrypted using aes and the user's master password
+// Each account can optionally have a link attached
+// Add an export button which will export the entire database of all users as a JSON file
+
+
 
 
