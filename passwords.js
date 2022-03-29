fce75208bfc29ea7bb710b5955d3214c1d2f2f1a
Shanmugadevan <shanmugadevan@protonmail.com>
2022-03-29 13:36:39 +0530

    Started working on passwordGenerate (not complete yet)

--- passwords.js
+++ passwords.js
@@ -26,6 +26,34 @@ function decryptAES(key, cipherText){
     return plainText
 }
 
-let key = keyDerivation("Thisisapassword!")
-let encryptedText = encryptAES(key, "Hello my name is shanmuga")
-let text = decryptAES(key, encryptedText)
+// let key = keyDerivation("Thisisapassword!")
+// let encryptedText = encryptAES(key, "Hello my name is shanmuga")
+// let text = decryptAES(key, encryptedText)
+
+
+function passwordGenerate(passwordLength){
+    // let data  = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,;:'"{}[]()-_=+*/&^%$#@!~?<>`.split("")
+    data = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ',', ';', ':', "'", '"', '{', '}', '[', ']', '(', ')', '-', '_', '=', '+', 
+    '*', '/', '&', '^', '%', '$', '#', '@', '!', '~', '?', '<', '>']
+    let password = ""
+    for (let i= 0 ; i<passwordLength;i++){
+        let num = crypto.getRandomValues(new Uint8Array(12))[0]
+        for (let j = 0; j<10;j++) {
+            if (num > 91){
+                num = num%91
+            }else{
+                j = 11
+            }
+        }
+        password += data[num]
+    }
+    // console.log(num)
+    console.log(password)
+    console.log(password.length)
+}
+
+passwordGenerate(25)
+
+
